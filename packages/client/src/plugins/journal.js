c9fedaad4f692b9c88c6ef3d03935ce0b69b6b7f
Andy Willis <awillis@fastmail.fm>
2022-08-28 00:13:28 +0100

    Plugin

--- packages/client/src/plugins/journal.js
+++ packages/client/src/plugins/journal.js
@@ -1,12 +1,4 @@
-import sanitiseText from '../helpers/sanitiseText.js';
-
-import {
-  common,
-  personalPronouns,
-  prepositions,
-  possessivePronouns,
-  stateVerbs,
-} from '../words/index.js';
+import { sanitiseText, filterText } from '../helpers/index.js';
 
 export default function journal(data) {
 
@@ -25,34 +17,8 @@ export default function journal(data) {
     }
 
     if (text.length) {
-
-      const fixed = text
-
-        // Join paras
-        .join('')
-
-        // Split on the space
-        .split(' ')
-
-        // Sort alphabetically
-        .sort((a, b) => a.localeCompare(b))
-
-        // Remove long words
-        .filter(word => word.length < 15)
-
-        // Remove common words
-        .filter(word => !common.includes(word))
-        .filter(word => !stateVerbs.includes(word))
-        .filter(word => !prepositions.includes(word))
-        .filter(word => !possessivePronouns.includes(word))
-        .filter(word => !personalPronouns.includes(word));
-
-      entries.push({
-        id,
-        title,
-        text: [ ...new Set(fixed) ].join(' ').trim().split(' ')
-      });
-
+      const filtered = filterText(text);
+      entries.push({ id, title, text: filtered });
     }
 
   }
