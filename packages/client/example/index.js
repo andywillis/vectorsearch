752565e59785ccffee8ae6156c69e554fe4bd85c
Andy Willis <awillis@fastmail.fm>
2022-08-28 18:12:21 +0100

    Example

--- packages/client/example/index.js
+++ packages/client/example/index.js
@@ -1,7 +1,7 @@
 import data from '../data/data.json' assert { type: 'json' };
-import journal from './plugins/journal.js';
+import journal from '../src/plugins/journal.js';
 
-import VectorSearch from './vectorSpace.js';
+import VectorSearch from '../src/index.js';
 
 new VectorSearch()
   .addPlugin(journal)
