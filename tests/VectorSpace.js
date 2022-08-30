4fb5773d3193ceae5a50fb127f395f1952e0bf86
Andy Willis <awillis@fastmail.fm>
2022-08-30 13:23:57 +0100

    Added new tests

---
+++ tests/VectorSpace.js
@@ -0,0 +1,14 @@
+import test from 'tape';
+import VectorSpace from '../src/index.js';
+
+const vs = new VectorSpace();
+
+test('VectorSpace', t => {
+
+  t.test('is an object', t => {
+    t.plan(1);
+    t.equal(typeof vs, 'object');
+    t.end();
+  });
+
+});
--- tests/sanitiseText.js
+++ tests/sanitiseText.js
@@ -7,36 +7,40 @@ import sanitiseText, {
   replacePeriodBackslash
 } from '../src/helpers/sanitiseText.js';
 
-test('sanitiseText is a function', t => {
-  t.plan(1);
-  t.equal(typeof sanitiseText, 'function');
-  t.end();
-});
+test('sanitiseText', t => {
 
-test('it removes HTML markup', t => {
-  t.plan(1);
-  const txt = '<h1>Hallo</h1>';
-  t.equal(removeHtmlMarkup(txt), 'Hallo');
-  t.end();
-});
+  t.test('is a function', t => {
+    t.plan(1);
+    t.equal(typeof sanitiseText, 'function');
+    t.end();
+  });
 
-test('it removes line endings', t => {
-  t.plan(1);
-  const txt = 'test\ntest2\n';
-  t.equal(removeLineendings(txt), 'testtest2');
-  t.end();
-});
+  t.test('it removes HTML markup', t => {
+    t.plan(1);
+    const txt = '<h1>Hallo</h1>';
+    t.equal(removeHtmlMarkup(txt), 'Hallo');
+    t.end();
+  });
 
-test('it removes punctuation', t => {
-  t.plan(1);
-  const txt = 'Aabc324;;;99((*)(*&)*';
-  t.equal(removePunctuation(txt), 'Aabc32499');
-  t.end();
-});
+  t.test('it removes line endings', t => {
+    t.plan(1);
+    const txt = 'test\ntest2\n';
+    t.equal(removeLineendings(txt), 'testtest2');
+    t.end();
+  });
 
-test('it replaces periods/backslashes with a space', t => {
-  t.plan(1);
-  const txt = 'This is a test. But! / What is this?';
-  t.equal(replacePeriodBackslash(txt), 'This is a test  But!   What is this?');
-  t.end();
+  t.test('it removes punctuation', t => {
+    t.plan(1);
+    const txt = 'Aabc324;;;99((*)(*&)*';
+    t.equal(removePunctuation(txt), 'Aabc32499');
+    t.end();
+  });
+
+  t.test('it replaces periods/backslashes with a space', t => {
+    t.plan(1);
+    const txt = 'This is a test. But! / What is this?';
+    t.equal(replacePeriodBackslash(txt), 'This is a test  But!   What is this?');
+    t.end();
+  });
+
 });
